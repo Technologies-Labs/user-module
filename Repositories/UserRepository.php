549bbbb880b88a12582ea29e366e10ce9dc9f0c1
Ammar050 <Ammar090@yandex.com>
2021-08-17 16:59:10 +0300

    BIAL-93 Added contact with website for users

--- Repositories/UserRepository.php
+++ Repositories/UserRepository.php
@@ -3,8 +3,14 @@
 namespace Modules\UserModule\Repositories;
 
 use Illuminate\Support\Facades\Auth;
+use Modules\UserModule\Entities\Contact;
+use App\Traits\UploadTrait;
+use Illuminate\Http\Request;
 
+use function PHPUnit\Framework\isNull;
 class UserRepository {
+
+    use UploadTrait;
 
 /**
      * Get User Notifications
@@ -17,5 +23,33 @@ class UserRepository {
         else if($type == "financial") {
         return Auth::user()->notifications->where('is_financial' , 1)->where('is_read' , 0)->sortByDesc('updated_at')->take(5);
         }
+    }
+
+/**
+     * Contact user with website
+*/
+
+    public function contact(Request $req) {
+
+        $req->validate($req, [
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'message'   => 'required',
+            'file'      => 'nullable',
+        ]);
+
+        if(!isNull($req->file)){
+            $file = $this->uploadOne($req->file , 'assets/files/contacts');
+        }
+        else {
+            $file = null;
+        }
+
+       return Contact::create([
+        'name'      => $req->name,
+        'email'     => $req->email,
+        'message'   => $req->message,
+        'file'      => $file,
+        ]);
     }
 }
