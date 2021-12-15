149b5182345f4e98085262656dcd2101e8a35b61
HaithamMofleh <moflehhaitham732@gmail.com>
2021-12-15 17:02:05 +0400

    Fix Users In Marketplace

--- Http/Livewire/User/SuggestedUsers.php
+++ Http/Livewire/User/SuggestedUsers.php
@@ -14,8 +14,12 @@ class SuggestedUsers extends Component
     public $template;
     public function mount()
     {
-        $users = app('services')->getUsersInServices(MarketplaceEnum::SUGGESTED_USERS)->users;
-        $this->users = $users->random($users->count()/2);
+        $users = app('services')->getUsersInServices(MarketplaceEnum::SUGGESTED_USERS);
+        if(!$users){
+            $this->users = [];
+            return;
+        }
+        $this->users = $users->users->random($users->users->count()/2);
     }
 
     public function render()
